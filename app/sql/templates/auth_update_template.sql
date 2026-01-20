b73474a89d93c1ef29711fd986be30e4d96e37f5
SEB\sbbla <sbblache@gmail.com>
2026-01-20 23:53:07 +0100

    fix database auth update template

--- app/sql/templates/auth_update_template.sql
+++ app/sql/templates/auth_update_template.sql
@@ -15,22 +15,5 @@ VALUES (1, 4, -1);
 -- Update realmlist
 DELETE FROM realmlist;
 
-INSERT INTO realmlist (
-    id, name, project_shortname, address, port, icon, color, timezone,
-    allowedSecurityLevel, population, gamebuild, flag,
-    project_hidden, project_enabled, project_dbname, project_dbworld,
-    project_dbarchive, project_rates_min, project_rates_max,
-    project_transfer_level_max, project_transfer_items,
-    project_transfer_skills_spells, project_transfer_glyphs,
-    project_transfer_achievements, project_server_same,
-    project_server_settings, project_server_remote_path,
-    project_accounts_detach, project_setskills_value_max,
-    project_chat_enabled, project_statistics_enabled
-) VALUES (
-    1, '${REALM_NAME}', '${REALM_NAME}', '${REALM_ADDRESS}', ${REALM_PORT},
-    ${REALM_ICON}, ${REALM_COLOR}, ${REALM_TIMEZONE},
-    ${REALM_SECURITY}, ${REALM_POP}, ${REALM_BUILD}, ${REALM_FLAG},
-    0, 1, '', '', '',
-    0, 0, 80, 'IGNORE', 'IGNORE', 'IGNORE', 'IGNORE',
-    0, '0', '0', 1, 0, 0, 0
-);
+INSERT INTO `realmlist` (`id`, `name`, `address`, `port`, `localAddress`, `localSubnetMask`, `icon`, `flag`, `timezone`, `allowedSecurityLevel`, `population`, `gamebuild`) VALUES 
+(1, '${REALM_NAME}', '${REALM_ADDRESS}', ${REALM_PORT}, '127.0.0.1', '255.255.255.0', ${REALM_ICON}, ${REALM_FLAG}, ${REALM_TIMEZONE}, ${REALM_SECURITY}, ${REALM_POP}, ${REALM_BUILD});
